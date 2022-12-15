ca8c08cb05a076a593c77d8825b5a4f6b6942c72
Idris Raja <idris.raja@gmail.com>
2022-12-14 20:19:46 -0600

    nix integration without touching stack.yaml

--- shell.nix
+++ shell.nix
@@ -1,7 +1,23 @@
-let pkgs = import <nixpkgs> { };
-in pkgs.mkShell {
+let
+  pkgs = import <nixpkgs> { };
 
-  buildInputs = [ pkgs.stack ];
+  stack-wrapped = pkgs.symlinkJoin {
+    name = "stack";
+    paths = [ pkgs.stack ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/stack \
+        --add-flags "\
+          --nix \
+          --no-nix-pure \
+          --nix-shell-file=nix/stack-integration.nix \
+        "
+    '';
+  };
+
+in pkgs.mkShell {
+  # Do NOT use `stack`, otherwise system dependencies like `zlib` are missing at compilation
+  buildInputs = [ stack-wrapped ];
   NIX_PATH = "nixpkgs=" + pkgs.path;
   shellHook = ''
     set -o vi
